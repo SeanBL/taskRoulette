eb93640607e1fdb586d42e78f55e642776178815
Sean Bristol-Lee <seanbristol81@gmail.com>
2023-05-02 22:40:15 -0700

    add links mainpage

--- task-roulette/src/App.js
+++ task-roulette/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Home from './components/home';
 import Mainpage from './components/mainpage';
+import Dailytask from './components/dailytask';
+import Newtask from './components/newtask';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 //What's the difference between BrowserRouter and CreateBrowswerRouter?
@@ -13,6 +15,14 @@ const router = createBrowserRouter([
   {
     path: "/mainpage",
     element: <Mainpage />
+  },
+  {
+    path: "/dailytask",
+    element: <Dailytask />
+  },
+  {
+    path: "/newtask",
+    element: <Newtask />
   }
 ])
 function App() {
